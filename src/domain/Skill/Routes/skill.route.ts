2eb15f4f35b1941c81e8b1c1e5467fadce5734dc
dev-Bambam <ayogood18@gmail.com>
2025-08-11 11:28:22 +0100

    I am done with the integration testing for the skill domain and am now moving on to the project domain and this shouldn't take me much time at all

--- src/domain/Skill/Routes/skill.route.ts
+++ src/domain/Skill/Routes/skill.route.ts
@@ -7,6 +7,6 @@ router.post("/create-skill", skillController.createSkill);
 router.get("/get-all-skill", skillController.fetchAllSkill);
 router.get("/get-a-skill/:skillId", skillController.fetchASkill);
 router.put("/update-a-skill/:skillId", skillController.updateASkill);
-router.delete("delete-skill/:skillId", skillController.deleteSkill);
+router.delete("/delete-skill/:skillId", skillController.deleteSkill);
 
 export default router;
