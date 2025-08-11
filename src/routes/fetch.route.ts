d7572fa360f2c7d1737e64fba9d1cadc7fdf2897
dev-Bambam <ayogood18@gmail.com>
2025-08-11 21:52:21 +0100

    I have separated the route to their domains

---
+++ src/routes/fetch.route.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { fetchAllSkill, fetchASkill } from "@src/domain/Skill/Controller/skill.controller";
+import {
+   getAllProjects,
+   getProjectById,
+   getProjectBySlug,
+} from "@src/domain/Project/Controller/project.controller";
+import { getBio } from "@src/domain/Bio/Controller/bio.controller";
+import { getDetails as getHowIBuiltThisDetails } from "@src/domain/how-i-built-this/how.controller";
+
+const router = Router();
+
+// Skill domain
+router.get("/skills", fetchAllSkill);
+router.get("/skills/:skillId", fetchASkill);
+
+// Project domain
+router.get("/projects", getAllProjects);
+router.get("/projects/:id", getProjectById);
+router.get("/projects/slug/:slug", getProjectBySlug);
+
+// Bio domain
+router.get("/bio", getBio);
+
+// How I Built This domain
+router.get("/how/get-details", getHowIBuiltThisDetails);
+
+export default router;
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -5,5 +5,6 @@ import frontendRoute from './frontend.route'
 const router = Router();
 
 router.use('/api/v1', adminRoute);
+router.use("/api/v1", frontendRoute);
 
 export default router;
