20750ef438d85aef949104159e2972366c2370d5
dev-Bambam <ayogood18@gmail.com>
2025-08-28 18:20:20 +0100

    I updated the route, so that I don't need to pass token for login

--- src/routes/all.route.ts
+++ src/routes/all.route.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import bioRoute from '../domain/Bio/Routes/bio.route';
 import uploadRouter from '@src/uploads/image/image.route';
-import authRouter from '@src/domain/auth/auth.route';
 import skillRouter from '@src/domain/Skill/Routes/skill.route'
 import projectRouter from "@src/domain/Project/Routes/project.route"
 import howRouter from '@src/domain/how-i-built-this/how.route'
@@ -10,7 +9,6 @@ const router = Router();
 
 router.use('/bio', bioRoute);
 router.use('/upload', uploadRouter);
-router.use('/auth', authRouter);
 router.use('/skill', skillRouter)
 router.use('/project', projectRouter)
 router.use('/how', howRouter)
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import adminRoute from './admin.routes';
 import frontendRoute from './frontend.route'
+import authRoute from '@src/domain/auth/auth.route'
 
 const router = Router();
 
 router.use('/api/v1', adminRoute);
 router.use("/api/v1", frontendRoute);
+router.use("/api/v1/auth", authRoute);
 
 export default router;
